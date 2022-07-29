f5a56f60db9d1ead42c841689a6ee86f1f7274dd
Juliya Sartasova <anjee2007@mail.ru>
2022-07-29 09:33:28 +0300

    День 4

--- script/index.js
+++ script/index.js
@@ -2,3 +2,5 @@ import './module/modal.js';
 import './module/menu.js';
 import './module/about-us-tabs.js';
 import './module/exchange-rates.js';
+import {accordion} from "./module/faq.js";
+accordion();
